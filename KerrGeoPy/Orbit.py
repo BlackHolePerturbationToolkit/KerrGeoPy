8ed0567d5050450947cc65965072ff8d9533d6cc
Seyong <seyongpark2001@gmail.com>
2023-06-16 15:08:33 -0400

    created orbit class

---
+++ KerrGeoPy/Orbit.py
@@ -0,0 +1,8 @@
+from kerrgeopy.constants import constants_of_motion
+from kerrgeopy.frequencies import orbital_frequencies
+
+class Orbit:
+    def __init__(self,a,p,e,x):
+        self.a, self.p, self.e, self.x = a, p, e, x
+        self.E, self.L, self.Q = constants_of_motion(a,p,e,x)
+        self.upsilon_r, self.upsilon_theta, self.upsilon_phi, self.gamma = orbital_frequencies(a,p,e,x)
