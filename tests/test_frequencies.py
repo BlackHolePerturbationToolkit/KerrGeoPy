db61eeb8319b970e8ad20529551c1bfcce0bac8a
Se Yong Park <seyongpark2001@gmail.com>
2023-07-12 15:46:37 -0400

    fixed tests

--- tests/test_frequencies.py
+++ tests/test_frequencies.py
@@ -9,31 +9,31 @@ DATA_DIR = THIS_DIR.parent / "tests/data"
 
 class TestFrequencies(unittest.TestCase):
     def test_extreme_kerr(self):
-        with self.assertRaises(ValueError): orbital_frequencies(1,12,0.5,0.5)
+        with self.assertRaises(ValueError): mino_frequencies(1,12,0.5,0.5)
         with self.assertRaises(ValueError): r_frequency(1,0.5,12,0.5)
         with self.assertRaises(ValueError): theta_frequency(1,0.5,12,0.5)
         with self.assertRaises(ValueError): phi_frequency(1,0.5,12,0.5)
 
     def test_polar(self):
-        with self.assertRaises(ValueError): orbital_frequencies(0.5,12,0.5,0)
+        with self.assertRaises(ValueError): mino_frequencies(0.5,12,0.5,0)
         with self.assertRaises(ValueError): r_frequency(0.5,0.5,12,0)
         with self.assertRaises(ValueError): theta_frequency(0.5,0.5,12,0)
         with self.assertRaises(ValueError): phi_frequency(0.5,0.5,12,0)
 
     def test_marginally_bound(self):
-        with self.assertRaises(ValueError): orbital_frequencies(0.5,12,1,0.5)
+        with self.assertRaises(ValueError): mino_frequencies(0.5,12,1,0.5)
         with self.assertRaises(ValueError): r_frequency(0.5,12,1,0.5)
         with self.assertRaises(ValueError): theta_frequency(0.5,12,1,0.5)
         with self.assertRaises(ValueError): phi_frequency(0.5,12,1,0.5)
 
     def test_invalid_arguments(self):
-        with self.assertRaises(ValueError): orbital_frequencies(2,5,0.5,0.5)
+        with self.assertRaises(ValueError): mino_frequencies(2,5,0.5,0.5)
         with self.assertRaises(ValueError): r_frequency(0.5,5,-0.5,0.5)
         with self.assertRaises(ValueError): theta_frequency(0.5,5,0.5,-2)
         with self.assertRaises(ValueError): phi_frequency(0.5,5,2,0.5)
 
     def test_unstable(self):
-        with self.assertRaises(ValueError): orbital_frequencies(0.5,5,0.5,0.5)
+        with self.assertRaises(ValueError): mino_frequencies(0.5,5,0.5,0.5)
         with self.assertRaises(ValueError): r_frequency(0.5,5,0.5,0.5)
         with self.assertRaises(ValueError): theta_frequency(0.5,5,0.5,0.5)
         with self.assertRaises(ValueError): phi_frequency(0.5,5,0.5,0.5)
@@ -41,7 +41,7 @@ class TestFrequencies(unittest.TestCase):
     def test_random(self):
         values = np.genfromtxt(DATA_DIR / "freq_values.txt",delimiter=",")
         mathematica_freq_output = np.genfromtxt(DATA_DIR / "mathematica_freq_output.txt")
-        python_freq_output = np.apply_along_axis(lambda x: orbital_frequencies(*x),1,values)
+        python_freq_output = np.apply_along_axis(lambda x: mino_frequencies(*x),1,values)
         for i, params in enumerate(values):
             with self.subTest(i=i,params=params):
                 self.assertTrue(np.allclose(abs(mathematica_freq_output[i]),abs(python_freq_output[i])))
