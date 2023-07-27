0cfded23a1d5f61bff7b7bcb543bcb4195b97c1a
Se Yong Park <seyongpark2001@gmail.com>
2023-07-27 15:46:03 -0400

    updated metadata

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name="kerrgeopy",
     version="0.0.1",
     author="Seyong Park",
-    description="Library for computing geodesics in Kerr spacetime",
+    description="Library for computing bound and plunging geodesics in Kerr spacetime",
     url="https://github.com/syp2001/KerrGeoPy",
     packages=setuptools.find_packages(),
     classifiers=(
