10f268b37705b14e6c3a34945adf1e573f3597c5
Seyong <seyongpark2001@gmail.com>
2023-10-03 11:54:28 -0400

    fixed image in readme

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setup(
     name="kerrgeopy",
-    version="0.1.0",
+    version="0.2.0",
     author="Seyong Park",
     description="Library for computing bound and plunging geodesics in Kerr spacetime",
     url="https://github.com/syp2001/KerrGeoPy",
