654a4c092f11642b4229a810264a0250eecb6697
Se Yong Park <seyongpark2001@gmail.com>
2023-11-01 15:05:44 -0400

    added description to github readme and updated repo url

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -58,7 +58,7 @@ exclude_patterns = []
 html_theme = 'sphinx_book_theme'
 
 html_theme_options = {
-    "repository_url": "https://github.com/syp2001/KerrGeoPy",
+    "repository_url": "https://github.com/BlackHolePerturbationToolkit/KerrGeoPy",
     "use_repository_button": True,
     "navbar_end": ["navbar-icon-links"],
 }
