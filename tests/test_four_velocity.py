0ba695bf8f0ea30a3d21d11bffc0aa26d61fb948
Seyong Park <seyongpark2001@gmail.com>
2023-11-27 16:57:19 -0500

    autoformatted unit test code

--- tests/test_four_velocity.py
+++ tests/test_four_velocity.py
@@ -9,107 +9,159 @@ THIS_DIR = Path(__file__).parent
 DATA_DIR = THIS_DIR.parent / "tests/data"
 
 stable_orbit_values = np.genfromtxt(DATA_DIR / "stable_orbit_parameters.txt", delimiter=",")
-complex_plunging_orbit_values = np.genfromtxt(DATA_DIR / "plunging_orbit_parameters_complex.txt", delimiter=",")
-real_plunging_orbit_values = np.genfromtxt(DATA_DIR / "plunging_orbit_parameters_real.txt", delimiter=",")
-plunging_orbit_values = np.concatenate((complex_plunging_orbit_values,real_plunging_orbit_values),axis=0)
+complex_plunging_orbit_values = np.genfromtxt(
+    DATA_DIR / "plunging_orbit_parameters_complex.txt", delimiter=","
+)
+real_plunging_orbit_values = np.genfromtxt(
+    DATA_DIR / "plunging_orbit_parameters_real.txt", delimiter=","
+)
+plunging_orbit_values = np.concatenate(
+    (complex_plunging_orbit_values, real_plunging_orbit_values), axis=0
+)
 times = np.genfromtxt(DATA_DIR / "stable_orbit_times.txt", delimiter=",")
+
 
 class TestFourVelocity(unittest.TestCase):
     def test_stable_orbit_four_velocity(self):
-        components = ["ut","ur","utheta","uphi"]
+        """
+        Test four_velocity method against output from the KerrGeodesics Mathematica library for a random set of orbits.
+        """
+        components = ["ut", "ur", "utheta", "uphi"]
 
         for i, orbit in enumerate(stable_orbit_values):
-            mathematica_trajectory = np.genfromtxt(DATA_DIR / f"four_velocity/trajectory{i}.txt", delimiter=",")
-            
-            a,p,e,x = orbit
-            stable_orbit = StableOrbit(a,p,e,x)
+            mathematica_trajectory = np.genfromtxt(
+                DATA_DIR / f"four_velocity/trajectory{i}.txt", delimiter=","
+            )
+
+            a, p, e, x = orbit
+            stable_orbit = StableOrbit(a, p, e, x)
             u_t, u_r, u_theta, u_phi = stable_orbit.four_velocity()
 
             python_trajectory = np.transpose(
-                np.apply_along_axis(lambda x: np.array([u_t(x),u_r(x),u_theta(x),u_phi(x)]),0,times)
+                np.apply_along_axis(
+                    lambda x: np.array([u_t(x), u_r(x), u_theta(x), u_phi(x)]), 0, times
                 )
-            
+            )
+
             for j, component in enumerate(components):
-                with self.subTest(i=i,
-                                  component=component,
-                                  params="a = {}, p = {}, e = {}, x = {}".format(*orbit),
-                                  diff=np.max(np.abs(mathematica_trajectory[:,j]-python_trajectory[:,j]))
-                                  ):
-                    self.assertTrue(np.allclose(mathematica_trajectory[:,j],python_trajectory[:,j],atol=1e-6))
-    
+                with self.subTest(
+                    i=i,
+                    component=component,
+                    params="a = {}, p = {}, e = {}, x = {}".format(*orbit),
+                    diff=np.max(np.abs(mathematica_trajectory[:, j] - python_trajectory[:, j])),
+                ):
+                    self.assertTrue(
+                        np.allclose(
+                            mathematica_trajectory[:, j], python_trajectory[:, j], atol=1e-6
+                        )
+                    )
+
     def test_norm(self):
-        times = np.linspace(0,10,10)
+        """
+        Check if the norm of the four-velocity is -1
+        """
+        times = np.linspace(0, 10, 10)
         # stable orbits
         for i, orbit in enumerate(stable_orbit_values):
-            a,p,e,x = orbit
-            stable_orbit = StableOrbit(a,p,e,x)
+            a, p, e, x = orbit
+            stable_orbit = StableOrbit(a, p, e, x)
             norm = stable_orbit._four_velocity_norm()
             for j, time in enumerate(times):
-                with self.subTest(i=i,
-                                params="a = {}, p = {}, e = {}, x = {}".format(*orbit),
-                                norm = norm(time)
-                                ):
-                    self.assertTrue(abs(norm(time)+1)<1e-8)
+                with self.subTest(
+                    i=i, params="a = {}, p = {}, e = {}, x = {}".format(*orbit), norm=norm(time)
+                ):
+                    self.assertTrue(abs(norm(time) + 1) < 1e-8)
 
         # plunging orbits
         for i, orbit in enumerate(plunging_orbit_values):
-            a,E,L,Q = orbit
-            plunging_orbit = PlungingOrbit(a,E,L,Q)
+            a, E, L, Q = orbit
+            plunging_orbit = PlungingOrbit(a, E, L, Q)
             norm = plunging_orbit._four_velocity_norm()
             for j, time in enumerate(times):
-                with self.subTest(i=i,
-                                params="a = {}, E = {}, L = {}, Q = {}".format(*orbit),
-                                norm = norm(time)
-                                ):
-                    self.assertTrue(abs(norm(time)+1)<1e-1)
+                with self.subTest(
+                    i=i, params="a = {}, E = {}, L = {}, Q = {}".format(*orbit), norm=norm(time)
+                ):
+                    self.assertTrue(abs(norm(time) + 1) < 1e-5)
 
     def test_using_numerical_differentiation(self):
-        components = ["ut","ur","utheta","uphi"]
+        """
+        Check that the four velocity computed using the geodesic equation matches with the
+        four velocity computed using numerical differentiation.
+        """
+        components = ["ut", "ur", "utheta", "uphi"]
 
-        #stable orbits
+        # stable orbits
         for i, orbit in enumerate(stable_orbit_values):
-            a,p,e,x = orbit
-            stable_orbit = StableOrbit(a,p,e,x)
+            a, p, e, x = orbit
+            stable_orbit = StableOrbit(a, p, e, x)
             u_t, u_r, u_theta, u_phi = stable_orbit.four_velocity()
             delta_t, delta_r, delta_theta, delta_phi = stable_orbit.numerical_four_velocity()
 
             analytic_four_velocity = np.transpose(
-                np.apply_along_axis(lambda x: np.array([u_t(x),u_r(x),u_theta(x),u_phi(x)]),0,times)
+                np.apply_along_axis(
+                    lambda x: np.array([u_t(x), u_r(x), u_theta(x), u_phi(x)]), 0, times
                 )
-            
-            numerical_four_velocity = np.transpose(
-                np.apply_along_axis(lambda x: np.array([delta_t(x),delta_r(x),delta_theta(x),delta_phi(x)]),0,times)
-                )
-            
-            for j, component in enumerate(components):
-                with self.subTest(i=i,
-                                  component=component,
-                                  params="a = {}, p = {}, e = {}, x = {}".format(*orbit),
-                                  diff=np.max(np.abs(analytic_four_velocity[:,j]-numerical_four_velocity[:,j]))
-                                  ):
-                    self.assertTrue(np.allclose(analytic_four_velocity[:,j],numerical_four_velocity[:,j],atol=1e-3))
+            )
 
-        #plunging orbits
+            numerical_four_velocity = np.transpose(
+                np.apply_along_axis(
+                    lambda x: np.array([delta_t(x), delta_r(x), delta_theta(x), delta_phi(x)]),
+                    0,
+                    times,
+                )
+            )
+
+            for j, component in enumerate(components):
+                with self.subTest(
+                    i=i,
+                    component=component,
+                    params="a = {}, p = {}, e = {}, x = {}".format(*orbit),
+                    diff=np.max(
+                        np.abs(analytic_four_velocity[:, j] - numerical_four_velocity[:, j])
+                    ),
+                ):
+                    self.assertTrue(
+                        np.allclose(
+                            analytic_four_velocity[:, j], numerical_four_velocity[:, j], atol=1e-3
+                        )
+                    )
+
+        # plunging orbits
 
         # for i, orbit in enumerate(plunging_orbit_values):
-        #     a,E,L,Q = orbit
-        #     plunging_orbit = PlungingOrbit(a,E,L,Q)
+        #     a, E, L, Q = orbit
+        #     plunging_orbit = PlungingOrbit(a, E, L, Q)
         #     u_t, u_r, u_theta, u_phi = plunging_orbit.four_velocity()
         #     delta_t, delta_r, delta_theta, delta_phi = plunging_orbit.numerical_four_velocity()
 
         #     analytic_four_velocity = np.transpose(
-        #         np.apply_along_axis(lambda x: np.array([u_t(x),u_r(x),u_theta(x),u_phi(x)]),0,times)
+        #         np.apply_along_axis(
+        #             lambda x: np.array([u_t(x), u_r(x), u_theta(x), u_phi(x)]), 0, times
         #         )
-            
+        #     )
+
         #     numerical_four_velocity = np.transpose(
-        #         np.apply_along_axis(lambda x: np.array([delta_t(x),delta_r(x),delta_theta(x),delta_phi(x)]),0,times)
+        #         np.apply_along_axis(
+        #             lambda x: np.array([delta_t(x), delta_r(x), delta_theta(x), delta_phi(x)]),
+        #             0,
+        #             times,
         #         )
-            
+        #     )
+
         #     for j, component in enumerate(components):
-        #         with self.subTest(i=i,
-        #                           component=component,
-        #                           params="a = {}, E = {}, L = {}, Q = {}".format(*orbit),
-        #                           diff=np.max(np.abs(analytic_four_velocity[:,j]-numerical_four_velocity[:,j]))
-        #                           ):
-        #             self.assertTrue(np.allclose(analytic_four_velocity[:,j],numerical_four_velocity[:,j],rtol=1e-3,atol=1e-1))
-    
+        #         with self.subTest(
+        #             i=i,
+        #             component=component,
+        #             params="a = {}, E = {}, L = {}, Q = {}".format(*orbit),
+        #             diff=np.max(
+        #                 np.abs(analytic_four_velocity[:, j] - numerical_four_velocity[:, j])
+        #             ),
+        #         ):
+        #             self.assertTrue(
+        #                 np.allclose(
+        #                     analytic_four_velocity[:, j],
+        #                     numerical_four_velocity[:, j],
+        #                     rtol=1e-3,
+        #                     atol=1e-1,
+        #                 )
+        #             )
--- tests/test_frequencies.py
+++ tests/test_frequencies.py
@@ -7,27 +7,53 @@ THIS_DIR = Path(__file__).parent
 
 DATA_DIR = THIS_DIR.parent / "tests/data"
 
+
 class TestFrequencies(unittest.TestCase):
     def test_extreme_kerr(self):
-        with self.assertRaises(ValueError): mino_frequencies(1,12,0.5,0.5)
+        """
+        Test that a ValueError is raised when a = 1.
+        """
+        with self.assertRaises(ValueError):
+            mino_frequencies(1, 12, 0.5, 0.5)
 
     def test_polar(self):
-        with self.assertRaises(ValueError): mino_frequencies(0.5,12,0.5,0)
+        """
+        Test that a ValueError is raised when x = 0.
+        """
+        with self.assertRaises(ValueError):
+            mino_frequencies(0.5, 12, 0.5, 0)
 
     def test_marginally_bound(self):
-        with self.assertRaises(ValueError): mino_frequencies(0.5,12,1,0.5)
+        """
+        Test that a ValueError is raised when e = 1
+        """
+        with self.assertRaises(ValueError):
+            mino_frequencies(0.5, 12, 1, 0.5)
 
     def test_invalid_arguments(self):
-        with self.assertRaises(ValueError): mino_frequencies(2,5,0.5,0.5)
+        """
+        Test that a ValueError is raised for invalid arguments.
+        """
+        with self.assertRaises(ValueError):
+            mino_frequencies(2, 5, 0.5, 0.5)
 
     def test_unstable(self):
-        with self.assertRaises(ValueError): mino_frequencies(0.5,5,0.5,0.5)
+        """
+        Test that a ValueError is raised when p is < separatrix(a,e,x)
+        """
+        with self.assertRaises(ValueError):
+            mino_frequencies(0.5, 5, 0.5, 0.5)
 
     def test_random(self):
-        values = np.genfromtxt(DATA_DIR / "freq_parameters.txt",delimiter=",")
+        """
+        Test mino_frequencies method against output from the KerrGeodesics Mathematica library for a random set of orbits.
+        """
+        values = np.genfromtxt(DATA_DIR / "freq_parameters.txt", delimiter=",")
         mathematica_freq_output = np.genfromtxt(DATA_DIR / "mathematica_freq_output.txt")
-        python_freq_output = np.apply_along_axis(lambda x: mino_frequencies(*x),1,values)
+        python_freq_output = np.apply_along_axis(lambda x: mino_frequencies(*x), 1, values)
         for i, params in enumerate(values):
-            with self.subTest(i=i,params=params):
+            with self.subTest(i=i, params=params):
                 E, L, Q = constants_of_motion(*params)
-                self.assertTrue(np.allclose(abs(mathematica_freq_output[i]),abs(python_freq_output[i])))
+                self.assertTrue(
+                    np.allclose(abs(mathematica_freq_output[i]), abs(python_freq_output[i]))
+                )
